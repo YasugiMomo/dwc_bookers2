ede348a3c5ead77c7b4ef6052cca7d901e793ba1
EC2 Default User <ec2-user@ip-172-31-18-54.ec2.internal>
2022-03-19 15:17:38 +0000

    [Update]投稿機能の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
    def after_sign_in_path_for(resource)
-     about_path
+    # sign in後にuser/showへ遷移するよう変更
+     _path
    end
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar))
-  end
-  
+   def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up,keys:[:name, :email])
+   end
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    # データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params) 
+    # データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # booksのindex画面へリダイレクト
+    redirect_to '/books'
   end
   
   def show
@@ -10,4 +20,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
